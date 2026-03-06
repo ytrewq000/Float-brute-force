766eba9e355f5283cccad3f94255081e707cc9b2
ytrewq000 <129806188+ytrewq000@users.noreply.github.com>
2026-03-05 22:22:12 -0800

    Add files via upload

---
+++ dev.py
@@ -0,0 +1,18 @@
+import numpy
+n = 0
+u = 1.797
+e = 308
+while True:
+    n += 1
+    p = numpy.longdouble(f"{u}{n}")
+    q = p *10**e
+    if q == numpy.longdouble('inf'):
+        n -= 1
+        u = numpy.longdouble(f"{u}{n}")
+        n = 0
+        print(u)
+    else:
+        print(q,p)
+    if len(str(p)) == 18:
+        break
+    
